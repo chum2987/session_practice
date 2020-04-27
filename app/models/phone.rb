2ddf26ab24390cd7fc9dee0ee4a3b78f51b97f84
chum2987 <chum2987@gmail.com>
2020-04-28 04:30:15 +0900

    セッション形式の会員登録実装

---
+++ app/models/phone.rb
@@ -0,0 +1,5 @@
+class Phone < ApplicationRecord
+  belongs_to :user, optional: true
+
+  validates :phone_number, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'phones', to: 'users/registrations#new_phone'
+    post 'phones', to: 'users/registrations#create_phone'
+  end
+  root to: "home#index"
 end
